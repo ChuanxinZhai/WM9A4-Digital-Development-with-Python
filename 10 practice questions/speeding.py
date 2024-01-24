d71bb4e1c5a814426e7cf46f8eb80481a7bb4ee5
ChessonZhai <94314784+ChuanxinZhai@users.noreply.github.com>
2024-01-24 23:48:02 +0000

    Add files via upload

---
+++ 10 practice questions/speeding.py
@@ -0,0 +1,54 @@
+"""
+Speeding Ticket Function
+超速罚单
+Objective:
+Write a function named 'speeding_ticket' that evaluates whether a driver should receive a speeding ticket based on their driving speed and a special condition (their birthday).
+
+Function Parameters:
+1. speed (integer): The driver's speed in mph (miles per hour).
+2. is_birthday (boolean): A flag indicating whether the driver is driving on their birthday. 判断是否是生日在开车
+
+Instructions:
+- The function should return one of three strings based on the driver's speed: "No Ticket", "Small Ticket", or "Big Ticket".
+                                                                                无罚单        小罚单        大罚单
+- If the driver's speed is 60 mph or less, they should receive "No Ticket".     60以下 
+- If the driver's speed is between 61 and 80 mph inclusive, they should receive a "Small Ticket".  61-80
+- If the driver's speed is 81 mph or more, they should receive a "Big Ticket".  81以上
+- On the driver's birthday, the speed limits for each ticket category are increased by 5 mph. For example, they can go up to 65 mph and still receive "No Ticket" if is_birthday is True.
+    生日开到65没罚单
+
+
+    判断Birthday生日True/False
+"""
+
+
+def speeding_ticket(speed, is_birthday):
+    # Your code goes here
+    # Implement the logic based on the driver's speed and birthday condition
+
+    # If birthday, increase 5
+    speed_limit_increase = 5 if is_birthday else 0
+
+    # Speed + Birthday (True/False)
+    if speed <= 60 + speed_limit_increase:
+        return "No Ticket"
+    elif 61 <= speed <= 80 + speed_limit_increase:
+        return "Small Ticket"
+    else:
+        return "Big Ticket"
+
+
+
+
+# Test cases
+print(speeding_ticket(60, False))  # Expected output: "No Ticket"
+print(speeding_ticket(75, False)) # Expected output: "Small Ticket"
+print(speeding_ticket(85, False))  # Expected output: "Big Ticket"
+print(speeding_ticket(65, True)) # Expected output: "No Ticket"
+print(speeding_ticket(85, True))  # Expected output: "Small Ticket"
+
+# Test float
+print(speeding_ticket(59.5, False))
+print(speeding_ticket(84.5, True))
+print(speeding_ticket(64.51231232312321312332131232131314543645709876322531445647858, True))
+
