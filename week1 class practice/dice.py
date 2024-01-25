c926521e731f4f2ad0ab17d396fa59f0992e2a8d
ChessonZhai <94314784+ChuanxinZhai@users.noreply.github.com>
2024-01-25 03:10:49 +0000

    Add files via upload

---
+++ week1 class practice/dice.py
@@ -0,0 +1,13 @@
+import random
+
+def roll_dice():
+    """
+    Roll a six-sided dice.
+    Returns a random integer between 1 and 6.
+    """
+    return random.randint(1, 6)
+
+print(roll_dice())  
+print(roll_dice())  
+print(roll_dice())  
+
