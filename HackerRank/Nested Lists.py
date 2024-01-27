a4b2547a6bbbf71c9f242bc119b57dd032c03f4a
ChessonZhai <94314784+ChuanxinZhai@users.noreply.github.com>
2024-01-27 02:53:26 +0000

    Update Nested Lists.py

--- HackerRank/Nested Lists.py
+++ HackerRank/Nested Lists.py
@@ -5,15 +5,11 @@ if __name__ == '__main__':
         score = float(input())
         students.append([name, score])
 
-    
     all_scores = [score for name, score in students]
 
-    
     second_lowest_score = sorted(list(set(all_scores)))[1]
 
-    
     second_lowest_students = [name for name, score in students if score == second_lowest_score]
 
-    
     for name in sorted(second_lowest_students):
         print(name)
