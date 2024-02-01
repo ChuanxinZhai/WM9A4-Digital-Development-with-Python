b48b41744e4d47bcf81711a5c64e6084ee846e8b
ChessonZhai <94314784+ChuanxinZhai@users.noreply.github.com>
2024-02-01 22:49:43 +0000

    Create itertools.product().py

---
+++ HackerRank/itertools.product().py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from itertools import product
+
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+
+result = list(product(A, B))
+
+print(*result)
