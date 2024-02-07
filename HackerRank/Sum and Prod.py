c84884d2e0114a1d83b921b68b17f0b403e33bf2
ChessonZhai <94314784+ChuanxinZhai@users.noreply.github.com>
2024-02-07 12:40:29 +0000

    Create Sum and Prod.py

---
+++ HackerRank/Sum and Prod.py
@@ -0,0 +1,22 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import numpy as np
+
+# N, M
+N, M = map(int, input().split())
+
+array_elements = []
+
+# read
+for _ in range(N):
+    row = list(map(int, input().split()))
+    array_elements.append(row)
+
+#  NumPy
+my_array = np.array(array_elements)
+
+# sum
+sum_result = np.sum(my_array, axis=0)
+
+product_result = np.prod(sum_result)
+print(product_result)
