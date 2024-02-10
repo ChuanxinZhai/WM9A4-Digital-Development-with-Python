f678ef1ff297c9c3854da0d4dc4b5c2d35ac038a
ChessonZhai <94314784+ChuanxinZhai@users.noreply.github.com>
2024-02-10 22:28:10 +0000

    Update Sum and Prod.py

--- HackerRank/Sum and Prod.py
+++ HackerRank/Sum and Prod.py
@@ -12,11 +12,12 @@ for _ in range(N):
     row = list(map(int, input().split()))
     array_elements.append(row)
 
-#  NumPy
+#  NumPy(np)
 my_array = np.array(array_elements)
 
 # sum
 sum_result = np.sum(my_array, axis=0)
+
 
 product_result = np.prod(sum_result)
 print(product_result)
